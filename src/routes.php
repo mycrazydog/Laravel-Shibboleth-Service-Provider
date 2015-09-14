41172f6d6264f3e41f686d79c40d3175138c7849
mycrazydog <mycrazydog@ymail.com>
2015-09-14 12:01:39 -0400

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -11,12 +11,3 @@ Route::get('/idp', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethControlle
 Route::get('/local', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@localCreate');
 // Login Callback (Local)
 Route::post('/local', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@localAuthorize');
-
-// Login Callback (Emulated)
-Route::get('emulated/idp', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
-// Login Callback (Emulated)
-Route::post('emulated/idp', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
-// Login Route (Emulated)
-Route::get('emulated/login', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogin');
-// Logout Route (Emulated)
-Route::get('emulated/logout', 'StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogout');
